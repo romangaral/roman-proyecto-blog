93203476e6d5e5f1b2c8bd570dd83ada4ad04ce4
roman <romangaral@gmail.com>
2022-06-07 12:29:16 +0200

    despliege 1

--- src/helpers/getEntradas.js
+++ src/helpers/getEntradas.js
@@ -1,5 +1,6 @@
 const getEntradas = async () => {
-    const url = "http://localhost:8080/entradas";
+    //const url = "http://localhost:8080/entradas";
+    const url = "https://romangarcia-back-mysql.herokuapp.com/entradas";
     const res = await fetch(url);
     const entradas = await res.json();
 
--- src/helpers/getUsuarios.js
+++ src/helpers/getUsuarios.js
@@ -1,5 +1,6 @@
 const getUsuarios = async () => {
-    const url = "http://localhost:8080/usuarios";
+    // const url = "http://localhost:8080/usuarios";
+    const url = "https://romangarcia-back-mysql.herokuapp.com/usuarios";
     const res = await fetch(url);
     const usuarios = await res.json();
 
